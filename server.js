616880109bd669168817470ed8d6b594a4114409
melissastan <melissastan91@gmail.com>
2022-10-10 11:31:04 -0400

    npm are installed and server is setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const {format} = require('date-fns');
+const nodemailer = require('nodemailer');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'animal shelter',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.use(session(sess));
+
+// in case we created some helper functions
+const hbs = exphbs.create({ helpers });
+
+// Inform Express.js which template engine we're using
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
