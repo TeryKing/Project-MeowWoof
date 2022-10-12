e88c8bd6af08c9d8f26f8c70906e63888010e13a
melissastan <melissastan91@gmail.com>
2022-10-12 19:06:06 -0400

    routes work with autoincrem

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,9 @@
-// const { format } = require("date-fns");
+import { format } from 'date-fns';
 
-// module.exports = {
-//     arrival_date: (date) => {
-//       // Format date as MM/dd/yyyy
-//      const today = format(new Date(),'MM.dd.yyyy');
-//      console.log(today)
-//     //   return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-//     //     // We add five years to the 'year' value to calculate the end date
-//     //     new Date(date).getFullYear() }`;
-//       // return date.toLocaleDateString();
-//     }
-//   };
+module.exports = {
+    arrival_date: (date) => {
+     const today = format(new Date(date),'MM.dd.yyyy');
+     console.log(today)
+      return today;     
+    }
+  };
