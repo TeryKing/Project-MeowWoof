f886944206988450b4b5231a5f2ee20d83e221db
TeryKing <tery_x3@hotmail.com>
2022-10-11 19:24:33 -0500

    main, filter, petcards are waiting for db GETs. working on volunteer dashboard

---
+++ public/js/pet.js
@@ -0,0 +1,20 @@
+const newFormHandler = async (event) => {
+
+  
+    if (pet_name && species && pet_breed && pet_age && pet_gender && pet_size) {
+      const response = await fetch(`/api/projects`, {
+        method: 'GET',
+        body: JSON.stringify({ pet_name, species,pet_breed, pet_age, pet_gender, pet_size}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Meep');
+      }
+    }
+  };
+  
