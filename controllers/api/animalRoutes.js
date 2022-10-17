f1e4079bfc85bf762ddb278d355082c26e90c00e
melissastan <melissastan91@gmail.com>
2022-10-17 02:42:55 -0400

    dashboard is functional now

--- controllers/api/animalRoutes.js
+++ controllers/api/animalRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Animal } = require('../../models');
 const checkAuth = require('../../utils/checkAuth');
 
-//Add animal
+//Add animal when the pet is getting surrender
 //add middleware
 router.post('/',async (req, res) => {
     try {
@@ -27,7 +27,7 @@ router.post('/',async (req, res) => {
     }
   });
   
-  //delete animal when its adopted
+  //delete animal when its adopted => press adopted btn (it will remove the animal from the db)
   //add middleware
   router.delete('/:animal_id', async (req, res) => {
     try {
@@ -49,9 +49,43 @@ router.post('/',async (req, res) => {
     }
   });
 
+//dashboard specifally in the aside of the unassigned animals
 //updates animal info
+//the voluteer id is hard coded to the current user id
+router.put('/:animal_id/current_user', async (req, res) => {
+  try {
+
+    const updateAnimal = await Animal.update(
+        {
+          species: req.body.species,
+          name: req.body.name,
+          gender: req.body.gender,
+          breed: req.body.breed,
+          age: req.body.age,
+          size: req.body.size,
+          arrival_date: req.body.arrival_date,
+          assigned_volunteer: req.session.user_id,
+        },
+        {
+          where: {
+            animal_id: req.params.animal_id,
+          },
+        }
+      );
+  // console.log("volunteerId", volunteerId)
+
+  // console.log("updatedAnimal", updateAnimal)
+
+    
+    res.status(200).json(updateAnimal);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+//volunteer id comes from the body (use this when you want to reassign the animal to someone else that is not the current user)
 router.put('/:animal_id', async (req, res) => {
     try {
+
         const updateAnimal = await Animal.update(
             {
               species: req.body.species,
@@ -69,8 +103,11 @@ router.put('/:animal_id', async (req, res) => {
               },
             }
           );
-        //comment out for now to see if routes work
-        //   return res.json(updateAnimal);
+      // console.log("volunteerId", volunteerId)
+
+      // console.log("updatedAnimal", updateAnimal)
+
+        
       res.status(200).json(updateAnimal);
     } catch (err) {
       res.status(500).json(err);
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,74 @@
-let checkbox = document.querySelector()
-let submitBtn = document.querySelector("#submit")
+// let checkbox = document.querySelector()
+// let submitBtn = document.querySelector("#submit")
 
-const assignVoltoPetHandler = async (event) => {
-    event.preventDefault();
-    const checkbox = document.querySelector()
-    cos
+// const assignVoltoPetHandler = async (event) => {
+//     event.preventDefault();
+//     const checkbox = document.querySelector()
+//     cos
+// }
+// const assignedAminal = document.querySelector('#project-name').value.trim();
+
+const addAnimaltoCare = async(animal_id) => {
+    // console.log(animal_id)
+    const response = await fetch(`/api/animal/${animal_id}/current_user`,
+        {
+            method: 'PUT',  
+            body: JSON.stringify({
+
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+    if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to fetch user id');
+      }
+
 }
+// }
+
+const unassignAnimaltoCare = async(animal_id) => {
+    // console.log(animal_id)
+    const response = await fetch(`/api/animal/${animal_id}`,
+        {
+            method: 'PUT',  
+            body: JSON.stringify({
+                assigned_volunteer: null
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+    if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to update assigned_volunteer');
+      }
+
+}
+
+const adoptedBtn = async(animal_id) => {
+    // console.log(animal_id)
+    const response = await fetch(`/api/animal/${animal_id}`,
+        {
+            method: 'DELETE',  
+            body: JSON.stringify({
+                
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+    if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to delete animal from database');
+      }
+
+}
+
