b9dc252b34992560aebc7e8940b74720d3963ae5
Tyler Hudson <tylerhudson96@gmail.com>
2022-10-17 21:41:30 -0400

    reverting to fix new issue

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,12 +11,10 @@ router.post('/', async (req, res) => {
         password: req.body.password,
         is_volunteer: req.body.is_volunteer
       });
-  console.log(req.body)
       req.session.save(() => {
-        req.session.user_id = newUserData.user_id ;
+        req.session.user_id = newUserData.user_id;
         req.session.logged_in = true;
-        req.session.is_volunteer = findUser.is_volunteer;
-        console.log(req.session)
+        req.session.is_volunteer = newUserData.is_volunteer;
   
         res.status(200).json(newUserData);
       });
