e77a942f39c96e4ab1b7597c872c885f1360ecf6
ripbog <crhinesmith5@gmail.com>
2022-10-18 18:33:46 -0400

    working on email changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const sgMail = require("@sendgrid/mail")
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -68,5 +70,51 @@ router.post('/logout', (req, res) => {
   }
 });
 
+// router.get('/email/current_user',async (req, res) => {
+//   try {
+//     const user = await User.
+
+//   } catch{
+
+//   }})
+router.post('/email', async (req, res) => {
+  //try catch with error
+  //use sequelize using the user ID that is stored in the session to then
+  console.log('Hello')
+  
+  try {  
+    console.log('this is being read 1')
+    console.log(req.session)
+    const userData = await User.findByPk(req.session.user_id, {
+    attributes: { exclude: ['password'] }
+  });
+    const user = userData.get({ plain: true });
+    console.log("This is user", user)
+    const msg = {
+      to: user.email,
+      from: "meowwoof.shelter@gmail.com",
+      subject: "Adoption Process Initiated",
+      text: `
+      Hello ${user.first_name}, 
+      You have begun the adoption process of one of the beautiful animals from MeowWoof Shelter! We are currently processing your request and reviewing your credentials as a potential adopter. This process usually takes between 5-10 business days depending on our request levels. We are excited that you are beginning the process!
+
+      -Best,
+      MeowWoof Family 
+      `
+      ,
+      html: `
+      <h3>Hello ${user.first_name},</h3> 
+      <p>You have begun the adoption process of one of the beautiful animals from MeowWoof Shelter! We are currently processing your request and reviewing your credentials as a potential adopter. This process usually takes between 5-10 business days depending on our request levels. We are excited that you are beginning the process!<br>
+      -Best,
+      MeowWoof Family </p>
+      `
+    }
+    sgMail.send(msg)
+    console.log('msg', msg)
+    res.send()
+  } catch (err) {
+    res.status(500).json(err);
+  }
+} )
 
 module.exports = router;
